6780f17c9f94173a532209246c955eb9eb4e6406
TanjilurNabil <nabil.ajkerdeal@gmail.com>
2022-01-08 02:13:33 +0600

    Usage of Components and hot loader

--- app/Main.js
+++ app/Main.js
@@ -1,19 +1,30 @@
+import { func } from "prop-types"
 import React from "react"
 import ReactDom from "react-dom"
 
 function OurApp() {
   return (
     <>
-      <h1>Our Amazing React app</h1>
-      <p>The Current time is {new Date().toLocaleString()}</p>
-      <small>This is Copyright Footer</small>
+      <OurHeader />
+      <OurContent />
+      <OurFooter />
     </>
   )
 }
-
+//Usage Of Components
+function OurHeader() {
+  return <h1>Our Amazing React app</h1>
+}
+function OurContent() {
+  return <p>The Current time is {new Date().toLocaleString()}</p>
+}
+function OurFooter() {
+  return <small>This is Copyright Footer</small>
+}
 setInterval(function () {
   ReactDom.render(<OurApp />, document.querySelector("#app"))
 }, 1000)
+//Load Just the content not the whole page if changes happen
 if (module.hot) {
   module.hot.accept()
 }
